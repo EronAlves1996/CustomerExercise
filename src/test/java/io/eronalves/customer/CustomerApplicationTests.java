8894379d55065052efff1c1a103b18f0bbaa39e5
Eron Alves <eron.a.silva@capgemini.com>
2022-12-06 23:50:36 -0300

    Succesfully test create

--- src/test/java/io/eronalves/customer/CustomerApplicationTests.java
+++ src/test/java/io/eronalves/customer/CustomerApplicationTests.java
@@ -1,13 +1,25 @@
 package io.eronalves.customer;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import io.eronalves.customer.model.Customer;
+import io.eronalves.customer.repositories.CustomerRepository;
 
 @SpringBootTest
 class CustomerApplicationTests {
 
+	@Autowired
+	CustomerRepository repository;
+
 	@Test
-	void contextLoads() {
+	void testCreate() {
+		Customer customer = new Customer();
+		customer.setId(1);
+		customer.setName("José");
+		customer.setEmail("jose@jose.com");
+		repository.save(customer);
 	}
 
 }
